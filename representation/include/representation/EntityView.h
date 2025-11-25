db634df8a9f061221978e17372f9f1754d72967b
yamanooo <hajahmadyaman@gmail.com>
2025-11-25 15:49:13 +0100

    little fix in EntityView.h

--- representation/include/representation/EntityView.h
+++ representation/include/representation/EntityView.h
@@ -10,7 +10,20 @@
 class EntityView: public Observer {
 private:
 public:
+  explicit EntityView(EntityModel* m) : model(m) {
+    if (model) model ->attach(this);
+  }
+  ~EntityView() override {
+    if (model) model->detach(this);
+  }
+  void update(GameEvent event) override {}
+
   //todo virtual draw();
+
+  // Prevent copying (views shouldn't be copied)
+  EntityView(const EntityView&) = delete;
+  EntityView& operator=(const EntityView&) = delete;
+
 protected:
   EntityModel* model;
 };
